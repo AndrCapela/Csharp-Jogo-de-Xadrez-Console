f051916549dc9bc957b40b267856dcd16cfb80a2
André Capela <andr.capela@gmail.com>
2018-09-08 00:19:33 -0300

    Criando a classe posicao xadrez

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -5,20 +5,24 @@ using Xadrez_Console.xadrez;
 namespace Xadrez_Console {
     class Program {
         static void Main(string[] args) {
-            try {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            /* try {
+                 Tabuleiro tab = new Tabuleiro(8, 8);
 
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 3));
+                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
+                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 3));
 
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch(TabuleiroException e) {
-                Console.WriteLine(e.Message);
-            }
+                 Tela.imprimirTabuleiro(tab);
+             }
+             catch(TabuleiroException e) {
+                 Console.WriteLine(e.Message);
+             }
+             */
 
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
             Console.ReadLine();
         }
     }
---
+++ Xadrez-Console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xadrez_Console.tabuleiro;
+
+
+namespace Xadrez_Console.xadrez {
+    class PosicaoXadrez {
+        public char coluna;
+        public int linha;
+
+        public PosicaoXadrez(char coluna, int linha) {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public override string ToString() {
+            return "" + coluna + linha;
+        }
+
+        public Posicao toPosicao() {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+    }
+}
