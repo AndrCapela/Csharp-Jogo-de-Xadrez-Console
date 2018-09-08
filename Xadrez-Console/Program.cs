07c13edf9e4b2c21c71b292bed470b3a08ca0e54
André Capela <andr.capela@gmail.com>
2018-09-07 23:42:57 -0300

    Impressao de tabuleiro

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -6,7 +6,11 @@ namespace Xadrez_Console {
     class Program {
         static void Main(string[] args) {
             Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(tab);
 
+
+
+            Console.ReadLine();
         }
     }
 }
--- Xadrez-Console/Tabuleiro/Tabuleiro.cs
+++ Xadrez-Console/Tabuleiro/Tabuleiro.cs
@@ -13,5 +13,8 @@ namespace Xadrez_Console.tabuleiro {
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
+        public Peca peca(int linha, int coluna) {
+            return pecas[linha, coluna];
+        }
     }
 }
